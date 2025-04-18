5727aeae003474c39036fbef874580ef0b200cb7
vanilla-ai-ml <anna.y.huang@student.uts.edu.au>
2025-04-19 08:30:33 +1000

    modified for testing

--- hazard_detection/pipelines/tasks/dowanload_libs.py
+++ hazard_detection/pipelines/tasks/dowanload_libs.py
@@ -1,14 +1,25 @@
-import torch
-from ultralytics import YOLO
+from clearml import Task
+# import torch
+# from ultralytics import YOLO
+# from enigmaai.config import Project, Config, ConfigFactory
 
 """
-Execute this so that it downloads the library dependecies 
-for the remote worker. 
+Downloads the library dependecies for the remote worker. 
+Run this on all remote workers so that other tasks can
+run smoothly instead of hanging trying to download dependcies
+at the same time.
 """
-task = Task.init(project_name="Hazard Detection", 
-                task_name="Download Dependencies", 
-                task_type=Task.TaskTypes.training)
 
-task.execute_remotely(queue_name="training")
+task = Task.init(project_name="Detection", 
+                task_name="Download Library Dependencies", 
+                task_type=Task.TaskTypes.custom)
+
+task.execute_remotely(queue_name="default")
 
 print("Downloaded packages to virutal env complete")
+
+
+# get project configurations
+# project = ConfigFactory.get_config(Project.HAZARD_DETECTION)
+# project_name = project.get('project-name')
+# project_name="Detection"
