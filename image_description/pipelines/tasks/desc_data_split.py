dd87cf922583a776270332bcee7ba7ba96340b78
Kamatchi <85495544+Kamatchi-23@users.noreply.github.com>
2025-05-01 12:51:59 +1000

    updated task code to split from base data

--- image_description/pipelines/tasks/desc_data_split.py
+++ image_description/pipelines/tasks/desc_data_split.py
@@ -1,21 +1,18 @@
 import os
-#import shutil
 from pathlib import Path
 import logging
 import json
 import sys
 from sklearn.model_selection import train_test_split
-from clearml import Task, Dataset, StorageManager
+from clearml import Task, Dataset
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 from enigmaai.config import Project, ConfigFactory
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 # get project configurations
 project = ConfigFactory.get_config(Project.SCENE_DESCRIPTION)
 project_name = project.get('project-name')
-
 
 task = Task.init(project_name=project_name, 
                 task_name="step5_desc_split_data", 
@@ -32,13 +29,16 @@ params = {
 task.connect(params)
 task.execute_remotely(queue_name="desc_preparation")
 
+"""
+Fetching captions dataset from task 3
+"""
 # download dataset 
 dataset_id = params['cap_dataset_id']
 dataset_name = params['cap_dataset_name']
 
 # validate task input params
 if not dataset_id and not dataset_name:
-    task.mark_completed(status_message="No dataset provided. Nothing to train on.")
+    task.mark_completed(status_message="No annotation dataset provided. Nothing to train on.")
     exit(0)
 if dataset_name: 
     # download the latest registered dataset
@@ -47,35 +47,23 @@ if dataset_name:
 extract_path = server_dataset.get_local_copy()          
 print(f"Downloaded dataset name: {server_dataset.name} id: ({server_dataset.id}) to: {extract_path}")
 extract_path = Path(extract_path)
-caption_file = extract_path / "desc_caption_dataset.json"
+
+caption_file = extract_path / project_name/ "desc_caption_basedataset.json"
 mapping = json.loads(open(caption_file, "r").read())
 logging.info(f"Loaded {len(mapping)} captions from {caption_file}")
-
-#get the image dataset from "Detection project- base_dataset"
-images_data = Dataset.get(
-    dataset_id="2231b5b121924ed684d6560cf6839619",
-    only_completed=True,
-    alias="base_images"  
-)
-images_root = Path(images_data.get_local_copy())
-images_dir  = images_root / "images"
-logging.info(f"Images downloaded to: {images_dir}")
-
-# Prepare list of stems
-all_stems = [Path(fn).stem for fn in mapping.keys()]
-
 
 """
 Split dataset after including reference descriptions, downloaded from ClearML server into train and val
 """
+# Prepare list of stems
+all_stems = [Path(fn).stem for fn in mapping.keys()]
+
 # split sizes
 val_size = params['val_size']
 random_state = params['random_state']
 
 # split train, val, test sets according task params
-train_stems, val_stems = train_test_split(all_stems, 
-                                         test_size = val_size, 
-                                         random_state=random_state)
+train_stems, val_stems = train_test_split(all_stems, test_size = val_size, random_state=random_state)
 # Build split mappings
 train_map = {f"{s}.jpg": mapping[f"{s}.jpg"] for s in train_stems}
 val_map   = {f"{s}.jpg": mapping[f"{s}.jpg"] for s in val_stems}
@@ -84,7 +72,7 @@ val_map   = {f"{s}.jpg": mapping[f"{s}.jpg"] for s in val_stems}
 Move files to train, val folders
 """
 # destination path for split dataset
-dest_path = Path(extract_path / "Desc_train_val_dataset")        
+dest_path = Path(extract_path / project_name/ "Desc_Split_dataset")        
 os.makedirs(dest_path, exist_ok=True)
 
 for split, split_map in [("train", train_map), ("val", val_map)]:
@@ -106,14 +94,14 @@ for split, split_map in [("train", train_map), ("val", val_map)]:
 # Upload the splits as a new ClearML dataset
 split_data = Dataset.create(
     dataset_project=project_name,
-    dataset_name="Desc_train_val_dataset"
+    dataset_name="Desc_Split_dataset"
 )
 split_data.add_files(str(dest_path))
 split_data.upload()
 split_data.finalize()
 logging.info(f"Uploaded split dataset id={split_data.id}")
 
-# 11) Save outputs
+# Save outputs
 task.set_parameter("output_dataset_project", split_data.project)
 task.set_parameter("output_dataset_id", split_data.id)
 task.set_parameter("output_dataset_name", split_data.name)
