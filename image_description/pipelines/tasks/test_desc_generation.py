beed890e5801d8c6c759010e68283046d6375dd6
Kamatchi <85495544+Kamatchi-23@users.noreply.github.com>
2025-05-01 23:00:29 +1000

    changed parameters

--- image_description/pipelines/tasks/test_desc_generation.py
+++ image_description/pipelines/tasks/test_desc_generation.py
@@ -51,8 +51,8 @@ task = Task.init(project_name=project_name,
 params = {
     'dataset_id': '',                # specific version of the dataset
     'dataset_name': 'Desc_Eval_Dataset',              # latest registered dataset
-    'base_dataset_id': 'e19da140dd6a479c864dd7bdf930918d',#'2231b5b121924ed684d6560cf6839619',     # specific version of the dataset
-    'base_dataset_name': 'eval_dataset_zip'
+    'eval_dataset_id': 'e19da140dd6a479c864dd7bdf930918d',#'2231b5b121924ed684d6560cf6839619',     # specific version of the dataset
+    'eval_dataset_name': 'eval_dataset_zip'
 }
 
 logger = task.get_logger()
@@ -61,8 +61,8 @@ task.execute_remotely(queue_name="desc_preparation")
 
 dataset_id = params['dataset_id']
 dataset_name = params['dataset_name']
-img_dataset_id = params['base_dataset_id']
-img_dataset_name = params['base_dataset_name']
+img_dataset_id = params['eval_dataset_id']
+img_dataset_name = params['eval_dataset_name']
 # validate task input params
 if not dataset_id and not dataset_name:
     task.mark_completed(status_message="No mapping dataset provided. Nothing to evaluate on. Ensure to execute task 2")
